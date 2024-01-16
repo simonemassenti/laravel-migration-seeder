022209a9cd2ddca4653b0e6327836995ffed9db5
simonemassenti <s.massenti96@gmail.com>
2024-01-16 14:08:50 +0100

    add: milestone 1

---
+++ database/migrations/2024_01_16_125036_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->date('data_partenza');
+            $table->time('oraio_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 6);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
